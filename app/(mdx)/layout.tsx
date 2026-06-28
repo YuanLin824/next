d9d48e3cfd274f1fee4e02a63b6d1e7153730f08
YuanLin <2752137708@qq.com>
2026-06-28 20:16:24 +0800

    feat((mdx)): ✨ 提交页面空白文档

---
+++ app/(mdx)/layout.tsx
@@ -0,0 +1,16 @@
+import { TableOfContents } from "@/components/table-of-contents"
+
+export default async function MDXLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <div className="flex">
+      <TableOfContents />
+      <article id="dmx-layout" className="px-3 pb-3">
+        {children}
+      </article>
+    </div>
+  )
+}
